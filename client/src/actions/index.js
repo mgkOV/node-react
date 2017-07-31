f477020fd1e7030ac966147eb8834cc9b062e5db
mgkOV <myagkov.mww@gmail.com>
2017-07-31 17:37:59 +0300

    refactor actions to new syntax

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
-  return async dispatch => {
-    const res = await axios.get('/api/current_user');
-    console.log(res.data);
-    dispatch({ type: FETCH_USER, payload: res.data });
-  };
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get('/api/current_user');
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
