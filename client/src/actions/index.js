250a9f57c84ec265a523768b8b32459fa034381e
mgkOV <myagkov.mww@gmail.com>
2017-07-31 17:23:49 +0300

    add fetchUser action creator

---
+++ client/src/actions/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import { FETCH_USER } from './types';
+
+export const fetchUser = () => {
+  return async dispatch => {
+    const res = await axios.get('/api/current_user');
+    console.log(res.data);
+    dispatch({ type: FETCH_USER, payload: res.data });
+  };
+};
